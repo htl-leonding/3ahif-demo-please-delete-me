e1b908e4feb49bbf8ebe9237696b58f514a54443
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-01-27 15:29:07 +0100

    config datasource for derbydb

--- demo-javafx/src/main/java/module-info.java
+++ demo-javafx/src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module at.htl.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.apache.derby.client;
+    requires org.apache.derby.commons;
+    requires org.apache.derby.tools;
 
     opens at.htl.demo to javafx.fxml;
     exports at.htl.demo;
+
+
 }
